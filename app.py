f3fca8f2224307e55ac6cd6bd038eb077cd54c98
Shashwat Aggarwal <shashwat4699@gmail.com>
2020-12-21 00:07:01 +0530

    app

--- app.py
+++ app.py
@@ -1,3 +1,46 @@
-from train_clf import load_model
+from train_clf import load_model, get_X_series
 
 clf = load_model(file_name='mlp_classifier.pkl')
+
+paths = ["Bengaluru-New Delhi", "Mumbai-Bengaluru", "Mumbai-Goa",
+         "Mumbai-New Delhi", "New Delhi-Goa", "New Delhi-Kolkata",  "New Delhi-Mumba"]
+airlines = ["Air India", "AirAsia", "Go Air", "IndiGo", "Spicejet", "Vistara"]
+days_to_depart = 1
+times = ["afternoon", "evening", "morning", "night"]
+
+if __name__ == "__main__":
+    print("Select flight path:")
+    for i, path in enumerate(paths):
+        print(i, path)
+    path_index = int(input())
+
+    print("Select airline index:")
+    for i, airline in enumerate(airlines):
+        print(i, airline)
+    airline_index = int(input())
+
+    print("Select time of departure:")
+    for i, time in enumerate(times):
+        print(i, time)
+    time_index = int(input())
+
+    print("Select day of departure: (Sunday = 0)")
+    day = int(input())
+
+    print("How many days to depart?")
+    days_to_depart = int(input())
+
+    print("Enter current fare:")
+    fare = float(input())
+
+    airline = airlines[airline_index]
+    path = paths[path_index]
+    day_time = f"{times[time_index]}-{day}"
+
+    X = get_X_series(airline, days_to_depart, path, day_time)
+
+    print(X)
+
+    y = clf.predict(X)
+
+    print(y)
--- train_clf.py
+++ train_clf.py
@@ -1,3 +1,4 @@
+import preprocess
 import numpy as np
 import pandas as pd
 import pickle
@@ -21,21 +22,21 @@ from sklearn.model_selection import validation_curve
 from tqdm.auto import tqdm
 tqdm.pandas()
 
-import preprocess
 
 def trainAndGenerateReport(X_train, X_test, y_train, y_test, clf, model_name):
     # Train Model
-    clf.fit(X_train,y_train)
+    clf.fit(X_train, y_train)
 
     # Calculate Accuracy
     print('Accuracy using {}'.format(model_name))
-    print('Training Accuracy: ', clf.score(X_train,y_train))
-    print('Testing Accuracy : ', clf.score(X_test,y_test))
+    print('Training Accuracy: ', clf.score(X_train, y_train))
+    print('Testing Accuracy : ', clf.score(X_test, y_test))
     print()
 
     # Precict on Train
     y_pred = clf.predict(X_test)
-    print(metrics.classification_report(y_test, y_pred, target_names=['Wait','Buy']))
+    print(metrics.classification_report(
+        y_test, y_pred, target_names=['Wait', 'Buy']))
 
     y_score = clf.predict_proba(X_test)[:, 1]
 
@@ -51,7 +52,7 @@ def trainAndGenerateReport(X_train, X_test, y_train, y_test, clf, model_name):
     plt.figure()
     lw = 2
     plt.plot(fpr, tpr, color='darkorange',
-            lw=lw, label='ROC curve (area = %0.2f)' % roc_auc)
+             lw=lw, label='ROC curve (area = %0.2f)' % roc_auc)
     plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
     plt.xlim([0.0, 1.0])
     plt.ylim([0.0, 1.05])
@@ -63,63 +64,79 @@ def trainAndGenerateReport(X_train, X_test, y_train, y_test, clf, model_name):
 
     return clf
 
+
 def save_model(model, file_name="model"):
-  pickle.dump(model, open(f'./Weights/{file_name}.pkl', 'wb'))
-  print("Model Saved!")
+    pickle.dump(model, open(f'./Weights/{file_name}.pkl', 'wb'))
+    print("Model Saved!")
+
 
 def load_model(file_name="model"):
-  model = pickle.load(open(f'./Weights/{file_name}', 'rb'))
-  print("Model Loaded!")
-  return model
+    model = pickle.load(open(f'./Weights/{file_name}', 'rb'))
+    print("Model Loaded!")
+    return model
+
+
+def get_X_series(airline, days_to_depart, path, day_time):
+    print(airline, path, day_time, days_to_depart)
+    return X.loc[(X[airline] == 1) & (X[path] == 1) & (X[day_time] == 1) & (X['days_to_depart'] == days_to_depart)]
+
 
 random_state = 69
-
 df = preprocess.preprocessClf()
 
-X = df.loc[:,df.columns != 'logical']
-X = X.loc[:,X.columns != 'std']
-X = X.loc[:,X.columns != 'logical_new']
+X = df.loc[:, df.columns != 'logical']
+X = X.loc[:, X.columns != 'std']
+X = X.loc[:, X.columns != 'logical_new']
 
 y = df['logical_new']
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=random_state)
+if __name__ == "__main__":
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.3, random_state=random_state)
 
-# Logistic Regression
-model_name = 'logistic_regression'
-clf = LogisticRegression(random_state=random_state,n_jobs=-1)
-clf = make_pipeline(StandardScaler(), clf, verbose=True)
+    # Logistic Regression
+    model_name = 'logistic_regression'
+    clf = LogisticRegression(random_state=random_state, n_jobs=-1)
+    clf = make_pipeline(StandardScaler(), clf, verbose=True)
 
-clf = trainAndGenerateReport(X_train, X_test, y_train, y_test, clf, model_name)
-save_model(clf, model_name)
+    clf = trainAndGenerateReport(
+        X_train, X_test, y_train, y_test, clf, model_name)
+    save_model(clf, model_name)
 
-# Random Forest Classifier
-model_name = 'random_forest_classifier'
-clf = RandomForestClassifier(max_depth=10, random_state=random_state, n_estimators=100)
-clf = make_pipeline(StandardScaler(), clf, verbose=True)
+    # Random Forest Classifier
+    model_name = 'random_forest_classifier'
+    clf = RandomForestClassifier(
+        max_depth=10, random_state=random_state, n_estimators=100)
+    clf = make_pipeline(StandardScaler(), clf, verbose=True)
 
-clf = trainAndGenerateReport(X_train, X_test, y_train, y_test, clf, model_name)
-save_model(clf, model_name)
+    clf = trainAndGenerateReport(
+        X_train, X_test, y_train, y_test, clf, model_name)
+    save_model(clf, model_name)
 
-# Gaussian NB Classifier
-model_name = 'gaussian_nb_classifier'
-clf = GaussianNB()
-clf = make_pipeline(StandardScaler(), clf, verbose=True)
+    # Gaussian NB Classifier
+    model_name = 'gaussian_nb_classifier'
+    clf = GaussianNB()
+    clf = make_pipeline(StandardScaler(), clf, verbose=True)
 
-clf = trainAndGenerateReport(X_train, X_test, y_train, y_test, clf, model_name)
-save_model(clf, model_name)
+    clf = trainAndGenerateReport(
+        X_train, X_test, y_train, y_test, clf, model_name)
+    save_model(clf, model_name)
 
-# MLP Classifier
-model_name = 'mlp_classifier'
-clf = MLPClassifier(hidden_layer_sizes=[64, 32, 16], learning_rate_init=0.01, random_state=random_state, max_iter=500)
-clf = make_pipeline(StandardScaler(), clf, verbose=True)
+    # MLP Classifier
+    model_name = 'mlp_classifier'
+    clf = MLPClassifier(hidden_layer_sizes=[
+                        64, 32, 16], learning_rate_init=0.01, random_state=random_state, max_iter=500)
+    clf = make_pipeline(StandardScaler(), clf, verbose=True)
 
-clf = trainAndGenerateReport(X_train, X_test, y_train, y_test, clf, model_name)
-save_model(clf, model_name)
+    clf = trainAndGenerateReport(
+        X_train, X_test, y_train, y_test, clf, model_name)
+    save_model(clf, model_name)
 
-# SVM Classifier/SVC
-model_name = 'svm_classifier'
-clf = SVC(gamma='auto', probability=True)
-clf = make_pipeline(StandardScaler(), clf, verbose=True)
+    # SVM Classifier/SVC
+    model_name = 'svm_classifier'
+    clf = SVC(gamma='auto', probability=True)
+    clf = make_pipeline(StandardScaler(), clf, verbose=True)
 
-clf = trainAndGenerateReport(X_train, X_test, y_train, y_test, clf, model_name)
-save_model(clf, model_name)
+    clf = trainAndGenerateReport(
+        X_train, X_test, y_train, y_test, clf, model_name)
+    save_model(clf, model_name)
